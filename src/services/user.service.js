b223949d0e2a71773a6ac8b1d3478bc58bef93e1
Shruthin Satheesh <vishnuofch@gmail.com>
2022-07-15 10:27:24 -0700

    to check re-registration

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,10 +4,16 @@ import bcrypt from 'bcrypt'
 
 //create new user
 export const newUser = async (body) => {
-  const saltRound = 10;
-  const hashPassword = bcrypt.hashSync(body.password, saltRound);
-  body.password = hashPassword
-  const data = await User.create(body);
-  return data;
+  const user = await User.findOne({ email: body.email })
+  console.log(user)
+  if (user === null) {
+    const saltRound = 10;
+    const hashPassword = bcrypt.hashSync(body.password, saltRound);
+    body.password = hashPassword
+    const data = await User.create(body);
+    return data;
+  } else {
+    throw new Error('Email is already registered')
+  }
 };
 
