d45abb1a21d30a481b40e8d4146fac8eef987d2d
Shruthin Satheesh <vishnuofch@gmail.com>
2022-07-14 17:56:21 -0700

    created signup api

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ import userRoute from './user.route';
  */
 const routes = () => {
   router.get('/', (req, res) => {
-    res.json('Welcome');
+    res.json('Welcome To Socially' );
   });
   router.use('/users', userRoute);
 
--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,14 +1,21 @@
 import Joi from '@hapi/joi';
+import HttpStatus from 'http-status-codes';
 
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
-    name: Joi.string().min(4).required()
+    firstName: Joi.string().min(4).required().error(Error('Enter a appropriate first name')),
+    lastName: Joi.string().min(4).required().error(Error('Enter a appropriate last name')),
+    email: Joi.string().email().required().error(Error('Enter a appropriate Email')),
+    password: Joi.string().min(6).required()
   });
-  const { error, value } = schema.validate(req.body);
+  const { error } = schema.validate(req.body);
   if (error) {
-    next(error);
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: `Enter valid deatils : ${error}`
+    })
   } else {
-    req.validatedBody = value;
+
     next();
   }
 };
