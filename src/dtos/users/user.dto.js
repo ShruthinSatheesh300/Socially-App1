d22f41a0bb780341ca1a3af5075ca2cdd9236d28
Shruthin Satheesh <vishnuofch@gmail.com>
2022-07-26 19:13:47 +0530

    removed unnecessary DTO

--- src/dtos/users/user.dto.js
+++ src/dtos/users/user.dto.js
@@ -5,9 +5,3 @@ export const UserDto = function (data) {
   this.email = data.email;
   this.createdAt = data.createdAt;
 };
-
-export const UserDtoL = function (data) {
-  this.firstName = data.firstName;
-  this.lastName = data.lastName;
-  this.email = data.email;
-};
